17a0e5fe955e809b16c8383c5a39463545f5b255
hiroki-usa <71242148+hiroki-usa@users.noreply.github.com>
2020-11-02 21:32:17 +0900

    Add files via upload

--- search-front/index.js
+++ search-front/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
-
+import { useState } from 'react';
+import Link from 'next/link'
+import { render } from "react-dom";
+import axios from 'axios'
+import React, { Component } from 'react';
 //react_チュートリアル↓
 //const name = 'usami';
 //const element = <h1>Hello,{name}</h1>
@@ -52,7 +56,6 @@ import Head from 'next/head'
 //   )
 // }
 
-
 const shops = [{ name: "ああああ", call: 111}, { name: "いいいい", call: 222}, { name: "うううう", call: 333}, { name: "ええええ", call: 444}];
 const shops2 = [{ name: "おおおお", call: 111}, { name: "いいいい", call: 222}, { name: "うううう", call: 333}, { name: "ええええ", call: 444}];
 let css1 = {backgroundColor:"#CCFFFF"};
@@ -60,89 +63,74 @@ let css2 = {backgroundColor:"#FFFFCC"};
 
 function ShopList(props){
   return(
-    <table>
-      <tr style={css1}>
-       <th>店名</th>
-       <th>電話番号</th>
-    </tr>
-     {props.shops.map((shop) => 
-      <tr style={css2} ><td>{shop.name}</td><td>{shop.call}</td></tr>
-      )}
-    </table>
+    <>
+      <table>
+        <tr style={css1}>
+        <th>店名</th>
+        <th>電話番号</th>
+       </tr>
+       {props.shops.map((shop) => 
+        <tr style={css2} ><td>{shop.name}</td><td>{shop.call}</td></tr>
+        )}
+      </table>
+    </>
   );
 }
 
-// export default function Home(){
-//   return(
-// <>
-// <form id ="form1" action="1">
-//   <label>
-//   ジャンル：
-//   <input type="text" id="input_message" placeholder="ここに入力"/>
-//   <input type="button" onclick="func1()" value="検索する"/>
-//   </label>
-// </form>
-// <style>{'table,th,td {border:1px solid black;}'}</style>
-// <ShopList shops={shops}></ShopList>
-// <ShopList shops={shops2}></ShopList>
-// </>
-//   )
-// }
-
-// import Link from 'next/link'
-// export default function OtherPage() {
-//     return (
-//         <>
-//             <h1>This is an other page.</h1>
-//             <Link href="/"><a>Go to a top page!</a></Link>
-//         </>
-//     );
-// }
-
-import { useState } from 'react';
-import Link from 'next/link'
-
-// const [shops, setShops] = useState(props.shops);
-// const addUshop =
-//     function () {
-//       const tempShops = JSON.parse(JSON.stringify(shops));
-//       tempShops.push({ name: "あああ", age: 20 })
-//       setShops(tempShops); 
-//     };
-//   return (
+class GenruForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {genru: ""};
+    this.handleChange = this.handleGenruChange.bind(this);
+  }
+  handleGenruChange(event) {
+    this.setState({genru: event.target.value});
+  }
+  render(){
+    return (
+      <form>
+        <label>
+        ジャンル：
+        <input type="text" value = {this.state.genru} placeholder="ここに入力" onChange = {this.handleGenruChange}/>
+        <input type="button" onClick={() => this.props.onClick()} value="検索する"/>
+        </label>
+      </form>
+    )
+  }
+}
 
 export default function Home() {
-  return (
-    <html>
-      <Head>
-        <style>{`table{border: 1px solid red;}`}</style>
-      </Head>
-      <body>
-      <form id ="form1" action="1">
-          <label>
-          ジャンル：
-          <input type="text" id="input_message" placeholder="ここに入力"/>
-          <input type="button" onclick="func1()" value="検索する"/>
-          </label>
-      </form>
-      <form>
-          <label>
-            店名:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            電話番号:
-            <input type="text" name="call" />
-          </label>
-          <button>追加</button>
-      </form>
-        <style>{'table,th,td {border:1px solid black;}'}</style>
-        <ShopList shops={shops}></ShopList>
+  console.log('test'); 
+    function loadGurunavi(props){
+      return(
+      axios.get('https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=276170a85746bbaefe88a4ce71f60084&name='+props.state.genru+'&pref=PREF13')
+      .then(function(response){
+        var restrans = restrans.data.rest;
+        const tempShops = [{}];
+        for (var n = 0; n < restrants.length; n++) {
+          tempShops.push({ name: restrants[n].name, call: restrants[n].tel });
+        }
+        setShops(tempShops)
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+          // handle error
+        console.log(error);
+      })
+      .finally(function () {
+          // always executed
+      })
+      )
+    }
 
-        <br></br>
-        <Link href="/other"><a>next shop list</a></Link>
-        
-      </body>
-    </html>
+  return (
+    <>
+      <GenruForm onClick={(genru) => loadGurunavi(this.props)} />
+      <style>{'table,th,td {border:1px solid black;}'}</style>
+      <ShopList shops={shops}></ShopList>
+      <br></br>
+      <Link href="/other"><a>next shop list</a></Link>
+    </>
   )
 }
